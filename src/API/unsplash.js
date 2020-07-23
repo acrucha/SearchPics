a3402503825bc23d1c38e17cb691bc04570284c5
acrucha <57640001+acrucha@users.noreply.github.com>
2020-07-22 21:01:10 -0300

    Update unsplash.js

--- src/API/unsplash.js
+++ src/API/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
         Authorization:
-            'Client-ID r9NqnLidh5pkV_v2kROB_joPV0-2yDN7QF7seahjHJI'
+            'Client-ID'
     }
 });
